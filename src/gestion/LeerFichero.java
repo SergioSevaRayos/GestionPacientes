691b010dfd7ff210ea42a1825f72c023be9bbbbe
SergioSevaRayos <sergiosevarayos@gmail.com>
2023-05-06 20:42:33 +0200

    V0.0

---
+++ src/gestion/LeerFichero.java
@@ -0,0 +1,34 @@
+package gestion;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+* En este método contemplamos otra opción de captura de 
+excepciones con la llamada en la definición
+* de un procedimmiento definido como estático con la palabra 
+reservada Throws seguida
+* de las excepciones a capturar
+* 
+*/
+public class LeerFichero {
+	public static void muestraContenido(String archivo) throws FileNotFoundException, IOException {
+		String cadena;
+		FileReader f = new FileReader(archivo);
+		BufferedReader b = new BufferedReader(f);
+		while ((cadena = b.readLine()) != null) {
+			System.out.println(cadena);
+		}
+		b.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		String rutaPac = "C:/Users/sergi/git/PracticaEDt6/ficheros/Pacientes.txt";
+		String rutaVis = "C:/Users/sergi/git/PracticaEDt6/ficheros/Visitas.txt";
+		muestraContenido(rutaPac);
+		System.out.println("--------------------------------------------------");
+		muestraContenido(rutaVis);
+	}
+}
---
+++ src/gestion/TratamientoFichero.java
@@ -0,0 +1,106 @@
+package gestion;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+
+/*
+ * Aquí contemplamos una de las opciones a la hora de capturar 
+ * excepciones.
+ * En esta parte lo hacemos con la estructura try + catch y añadimos la 
+ * opción de ejecución obligatoria con la sentencia finally.
+ */
+
+/**
+ * 
+ * @author Sergio Seva Rayos
+ * @version 1.0
+ *
+ */
+public class TratamientoFichero {
+	// Variables para leer los ficheros pacientes
+	static String rutaPac = "C:/Users/sergi/git/PracticaEDt6/ficheros/Pacientes.txt";
+	static String rutaVis = "C:/Users/sergi/git/PracticaEDt6/ficheros/Visitas.txt";
+	
+	// Variables para escribir en los ficheros
+	static PrintWriter pw = null;
+	static FileWriter fichero = null;
+
+	/**
+	 * Método por el cual guardamos los datos de los pacientes en un documento .txt
+	 */
+	public static void tryCatchPaci() {
+		try {
+			// Añadir flag a true para no machacar contenido del
+			fichero = new FileWriter(rutaPac, true);
+			pw = new PrintWriter(fichero);
+			
+			//---------------------------------------------- Inicio 
+			pw.println(Pacientes.DNI + ", " + Pacientes.nombre + ", " + Pacientes.edad + ", " + Pacientes.calle + ", " + Pacientes.localidad + ", " + Pacientes.cod_postal);
+			//---------------------------------------------- Fin   
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// Nuevamente aprovechamos el finally para
+				// asegurarnos que se cierra el fichero.
+				if (null != fichero) {
+					fichero.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+
+			} finally {
+				try {
+					// Nuevamente aprovechamos el finally para
+					// asegurarnos que se cierra el fichero.
+					if (null != fichero) {
+						fichero.close();
+					}
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Método por el cual guardamos los datos de las consultas en un documento .txt
+	 */
+	public static void tryCatchVisi() {
+		try {
+			// Añadir flag a true para no machacar contenido del
+			fichero = new FileWriter(rutaVis, true);
+			pw = new PrintWriter(fichero);
+			
+			//---------------------------------------------- Inicio 
+			pw.println(Pacientes.DNI + ", " + Visitas.f + ", " + Visitas.h + ", " + Visitas.peso + Visitas.kg + ", " + Visitas.altura + Visitas.m + ", " + ((double)Math.round(Visitas.IMC * 100d) / 100d));
+			//---------------------------------------------- Fin   
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// Nuevamente aprovechamos el finally para
+				// asegurarnos que se cierra el fichero.
+				if (null != fichero) {
+					fichero.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+
+			} finally {
+				try {
+					// Nuevamente aprovechamos el finally para
+					// asegurarnos que se cierra el fichero.
+					if (null != fichero) {
+						fichero.close();
+					}
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+		}
+	}
+}
