25efeb3f82cf3dfe29caae67e4797890febcf6d9
SergioSevaRayos <sergiosevarayos@gmail.com>
2023-05-06 20:51:45 +0200

    V0.0

---
+++ src/gestion/Main.java
@@ -0,0 +1,12 @@
+package gestion;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
