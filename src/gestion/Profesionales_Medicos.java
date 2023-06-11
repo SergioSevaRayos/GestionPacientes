87f382eca129041f84fc00470154bf4a36585200
SergioSevaRayos <sergiosevarayos@gmail.com>
2023-06-11 21:43:05 +0200

    V0.6 Se han añadido las clases para testear

--- src/gestion/Profesionales_Medicos.java
+++ src/gestion/Profesionales_Medicos.java
@@ -37,16 +37,6 @@ public class Profesionales_Medicos {
 		DNI = dni;
 		localidad = loc;
 	}
-	/**
-	 * Este método se encarga de insertar en la base de datos varios profesionales médicos.
-	 * Se utilizan diferentes llamadas al método "TestConexion.insert_Profesional" para insertar los registros.
-	 */
-	public static void insert_medicos() {
-		TestConexion.insert_Profesional(1, "111111111","Cirujia","Pepe","Elda","33333333o","Serna");
-		TestConexion.insert_Profesional(2, "222222222","Cardiologia","Maria","Alicante","44444444p","Gonzalez");
-		TestConexion.insert_Profesional(3, "333333333","Ginicologia","Jose","Ceuta","55555555p","Pinto");
-		TestConexion.insert_Profesional(4, "444444444","General","Vicente","Alicante","66666666o","Jimenez");
-		TestConexion.insert_Profesional(5, "555555555","Podologia","Andres","Cadiz","99999999y","Armario");
-	}
+
 	
 }
